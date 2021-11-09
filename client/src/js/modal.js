e75a0805d5682531be031c0843f48fd3786cdfd7
Yury <gigikalo57@gmail.com>
2021-11-09 19:57:38 +0200

    add modal mask big-head

---
+++ client/src/js/modal.js
@@ -0,0 +1,38 @@
+import MicroModal from 'micromodal';
+
+MicroModal.init({
+  onShow: modal => console.info(`${modal.id} is shown`), // [1]
+  onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+  openTrigger: 'data-custom-open', // [3]
+  closeTrigger: 'data-custom-close', // [4]
+  disableScroll: true, // [5]
+  disableFocus: false, // [6]
+  awaitOpenAnimation: false, // [7]
+  awaitCloseAnimation: false, // [8]
+  debugMode: true, // [9]
+})
+// ищем все кнопки-откывашки модалов
+const buttons = Array.from(document.getElementsByClassName('js-modal-trigger'))
+// если они есть
+if (buttons.length) {
+  // идем по все
+  buttons.forEach(button => {
+    // на каждую навешиваем прослушиватель клика
+    button.addEventListener('click', e => {
+      // убираем стандартное поведение кнопки
+      e.preventDefault()
+      // у кнопки есть дата-атрибут с именем нужного модала
+      const modalEntity = document.getElementById(button.dataset.micromodalTrigger)
+      console.log(modalEntity)
+      // находим контейнер, куда встатим найденный код
+      const mymodal = document.getElementById('innerModal')
+      console.log(mymodal)
+      // вставляем в готовый контейнер - html искомого модала
+      mymodal.innerHTML = modalEntity.innerHTML
+      // открываем наш модал
+      MicroModal.show(button.dataset.micromodalTrigger)
+
+      document.dispatchEvent(new Event('dom-bind'))
+    })
+  })
+}
